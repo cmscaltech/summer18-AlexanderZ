56b395677477ecdbddb152a793a8b83eed807157
Alexander <alexander.z2718@gmail.com>
2018-08-06 16:50:33 -0700

    updated file transferring

--- headnode/file_transfer.py
+++ headnode/file_transfer.py
@@ -1,16 +1,15 @@
 import argparse
 import os
 parser = argparse.ArgumentParser()
-parser.add_argument('-n', '--num', help='number for file to save as', type=int)
-parser.add_argument('-t', '--total', help='total number of files', type=int)
+parser.add_argument('-n', '--num', help='batch number', type=int)
+parser.add_argument('-t', '--total', help='total number of files (batch size)', type=int)
 args = parser.parse_args()
 
-numFiles = len(allFiles)
-for i in range(numFiles):
-    f = open('./surf2018/f' + str(i) '.txt', 'r')
+for i in range(args.total):
+    f = open('./surf2018/f' + str(i) + '.txt', 'r')
     contents = f.read()
     print 'contents ' + contents
     f.close()
-    f = open((args.num*numFiles + i) + '.txt', 'w+')
+    f = open(str(args.num*args.total + i) + '.txt', 'w+')
     f.write(contents)
     f.close()
